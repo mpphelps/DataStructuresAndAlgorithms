7b5472c3a6e2890d0337cfe131363c8d564bd299
Michael Phelps <Michael.Phelps@Honeywell.com>
2022-08-21 11:18:39 -0400

    Started graphs

---
+++ 10_Graphs_CSharp/10_Graphs_CSharp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Graphs
+{
+
+    class Graph
+    {
+        private class Node
+        {
+            private string _label;
+            public Node(string label)
+            {
+                _label = label;
+            }
+            public override string ToString()
+            {
+                return _label;
+            }
+        }
+
+        // need a hashmap for every label and it's index in the adjaceny list
+        // need an ajacency list which is an array of linked lists
+
+
+        public void AddNode(string label)
+        {
+
+        }
+
+        public void RemoveNode(string label)
+        {
+
+        }
+
+        public void AddEdge()
+        {
+
+        }
+
+        public void RemoveEdge()
+        {
+
+        }
+
+        public void Print()
+        {
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+}
+
+
+
