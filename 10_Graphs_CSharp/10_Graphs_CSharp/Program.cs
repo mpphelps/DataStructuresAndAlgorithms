b63727ac37c68a6cce73401270d75f914d51ee68
Michael Phelps <mpphelps@gmail.com>
2022-08-22 07:56:46 -0400

    Added add node and add edge to graphs

--- 10_Graphs_CSharp/10_Graphs_CSharp/Program.cs
+++ 10_Graphs_CSharp/10_Graphs_CSharp/Program.cs
@@ -19,14 +19,28 @@ namespace Graphs
                 return _label;
             }
         }
-
         // need a hashmap for every label and it's index in the adjaceny list
+        private Dictionary<string, Node> _nodes = new Dictionary<string, Node>();
         // need an ajacency list which is an array of linked lists
+        private Dictionary<Node, List<Node>> adjacencyList = new Dictionary<Node, List<Node>>();
 
 
         public void AddNode(string label)
         {
+            var node = new Node(label);
+            _nodes.Add(label, node);
+            adjacencyList.Add(node, new List<Node>());
+        }
+        public void addEdge(string from, String to)
+        {
+            var fromNode = _nodes[from];
+            if (fromNode == null)
+                throw new Exception("Illegal From Arguement");
+            var toNode = _nodes[to];
+            if (toNode == null)
+                throw new Exception("Illegal To Arguement");
 
+            adjacencyList[fromNode].Add(toNode);
         }
 
         public void RemoveNode(string label)
@@ -46,7 +60,18 @@ namespace Graphs
 
         public void Print()
         {
-
+            foreach (var source in adjacencyList.Keys)
+            {
+                var targets = adjacencyList[source];
+                if (targets.Count > 0)
+                {
+                    Console.WriteLine($"{source} is connected to: ");
+                    foreach (var target in targets)
+                    {
+                        Console.WriteLine($"{target}, ");
+                    }
+                }
+            }
         }
 
     }
@@ -54,7 +79,20 @@ namespace Graphs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myGraph = new Graph();
+            myGraph.AddNode("Andy");
+            myGraph.AddNode("Barbara");
+            myGraph.AddNode("Ed");
+            myGraph.AddNode("Michael");
+            myGraph.AddNode("Sofia");
+            myGraph.AddNode("Juliana");
+            myGraph.addEdge("Andy", "Ed");
+            myGraph.addEdge("Barbara", "Ed");
+            myGraph.addEdge("Andy", "Michael");
+            myGraph.addEdge("Barbara", "Michael");
+            myGraph.addEdge("Michael", "Juliana");
+            myGraph.addEdge("Sofia", "Juliana");
+            myGraph.Print();
         }
     }
     
