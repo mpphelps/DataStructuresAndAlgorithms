d90c6fdd375bf5d6dda07ecb4d7cc5a30a412786
Michael Phelps <Michael.Phelps@Honeywell.com>
2022-08-31 15:37:43 -0400

    Add linear search algorithm

---
+++ 13_SearchingAlgorithms_CSharp/13_SearchingAlgorithms_CSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Searching_Algorithms
+{
+    class SearchingAlgorithms
+    {
+        public static bool LinearSearch(int[] array, int searchItem)
+        {
+            /*
+             * Linear Search
+             *  iteratively go through an array comparing each item
+             *
+             * Time Complexity: Best: O(1), Worst O(n)
+             */
+
+            foreach (var item in array)
+            {
+                if (searchItem == item) return true;
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Linear Search
+
+            Console.WriteLine("Linear Search");
+            var array1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var searchItem1 = 4;
+            var searchItem2 = 90;
+            Console.WriteLine($"Is {searchItem1} in array? {SearchingAlgorithms.LinearSearch(array1, searchItem1)}");
+            Console.WriteLine($"Is {searchItem2} in array? {SearchingAlgorithms.LinearSearch(array1, searchItem2)}");
+
+            #endregion
+        }
+    }
+}
